e1c9e52344906369e4bbcb208f2d324d73d23c0a
Joseph Stadum <Joseph.Stadum@RPI-KS.com>
2025-04-28 17:20:25 -0500

    can detect if Label Traxx is running

--- login.py
+++ login.py
@@ -1,13 +1,15 @@
 import psutil
 
 
-process_name = "Label Traxx Client.exe"
+#program_name = "Label Traxx Client.exe"
 
-def detect_label_traxx():
+def detect_label_traxx(process_name):
     for proc in psutil.process_iter(['name']):
-        if proc.info['name'] and proc.info['name'].lower() in process_name.lower():
+        if proc.info['name'] and proc.info['name'].lower() == process_name.lower():
             return True
-        return False
+    return False
 
-print(detect_label_traxx())
+print(detect_label_traxx("Label Traxx Client.exe"))
+
+
 
