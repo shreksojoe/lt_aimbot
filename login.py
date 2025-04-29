b6d5a7d4c372097eac1d22fcfdf47fad54b0e0c8
Joseph Stadum <Joseph.Stadum@RPI-KS.com>
2025-04-29 10:27:52 -0500

    add functions for getting handles, title, waiting on programs to run etc

--- login.py
+++ login.py
@@ -3,10 +3,12 @@ import pygetwindow as gw
 import win32gui
 import win32process
 import win32con
+import subprocess
+import time
 
 
 # detect if label traxx is running
-def detect_label_traxx(process_name):
+def detect_process(process_name):
     for proc in psutil.process_iter(['name']):
         if proc.info['name'] and proc.info['name'].lower() == process_name.lower():
             return True
@@ -22,7 +24,7 @@ def get_pid_from_process_name(process_name):
     
 
 # get window handle for labeltraxx (so we can get the title)
-def get_hwnd_from_pid(pid):
+def get_hwnd_or_title_from_pid(pid, output):
 
     # verify that the window has a handle
     for window in gw.getAllWindows():
@@ -36,29 +38,70 @@ def get_hwnd_from_pid(pid):
         hwnd = window._hWnd
         _, win_pid = win32process.GetWindowThreadProcessId(hwnd)
         if win_pid == pid:
-            return window._hWnd
+            if output.lower() == "title":
+                return window.title
+            elif output.lower() == "handle":
+                return window._hWnd
+            else:
+                print(f"{output} wasn't an option. Please pick handle or title")
 
     return None
 
 
 # bring lt to the front
-def select_label_traxx(lt_title):
+def select_program(window_handle):
 
-    windows = gw.getWindowsWithTitle(lt_title)
-    lt_instance = detect_label_traxx(lt_title)
-        
-    if lt_instance:
-        for window in gw.getAllWindows():
-            if window.title and title.lower() in window.title.lower():
-                if window.isMinimized:
-                    window.restore
-                window.activate
-    else:
-        print("Label Traxx is not open")
-    
+    win32gui.ShowWindow(window_handle, win32con.SW_RESTORE)
+    win32gui.SetForegroundWindow(window_handle)
 
-label_traxx = "Label Traxx Client.exe"
-lt_pid = get_pid_from_process_name(label_traxx)
-lt_hwnd = get_hwnd_from_pid(lt_pid)
-print(lt_hwnd)
+# start a program
+def start_program(program_path): subprocess.Popen([program_path])
+
+# detect if lt window is selected
+
+# wait for program 
+def wait_for_program(pid, timeout=30):
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if win32gui.GetWindowText(win32gui.GetForegroundWindow()) == '':
+            print("Program is opened")
+            return
+        else:
+            exit
+        time.sleep(0.5)
+    print("Program Timed Out")
+
+# variables
+lt_process_name = "Label Traxx Client.exe"
+lt_path = r"C:\\Program Files\\LT Client\\Label Traxx Client.exe"
+lt_pid = get_pid_from_process_name(lt_process_name)
+lt_hwnd = get_hwnd_or_title_from_pid(lt_pid, "handle")
+lt_title = get_hwnd_or_title_from_pid(lt_pid, "title")
+
+
+# logic
+if detect_process(lt_process_name):
+    select_program(lt_hwnd)
+    if "Home Page" in lt_title:
+        print("Our work here is done")
+    #else:
+        #navigate to the home button and click it
+else:
+    start_program(lt_path)
+    wait_for_program(lt_pid)
+    # log into program
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
