61cc91bdc43d684e3f806d2d136b09e791854d9c
Joseph Stadum <Joseph.Stadum@RPI-KS.com>
2025-05-21 16:50:52 -0500

    fix some wierd errors

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -6,6 +6,8 @@ import csv
 import sys
 import os
 
+# Global variables:
+ticket_structure_is_done = False
 
 # Opens the csv, and stores rows in array 
 def csv_rows_to_array(input_csv):
@@ -25,6 +27,25 @@ def type_keyboard(text):
     keyboard.write(text)
     time.sleep(0.2)
 
+def make_ticket_structure(key, value, element):
+    print(f"key after testing: {key}")
+    print(f"value after testing: {value}")
+    if key == "Coordinates": # works
+        move_mouse(value)
+        time.sleep(0.5)
+    elif key == "Select All":
+        for _ in range(9):
+            pyautogui.press('backspace')
+        time.sleep(0.2)
+        action_is_done = True
+    elif not value:
+        print(f"just about to print: {element}")
+        type_keyboard(element)
+        action_is_done = True
+    else:
+        ticket_structure_is_done = True
+        print(f"ticket struct: {ticket_structure_is_done}")
+
 def csv_into_json(user_csv, user_json):
     action_is_done = False
     csv_rows = csv_rows_to_array(user_csv)
@@ -40,43 +61,30 @@ def csv_into_json(user_csv, user_json):
     with open (user_json, 'r') as json_instructions:
         json_step = json.load(json_instructions)
     
-        for order in csv_rows: # first loops through the order (x2)
-            print(f"order: {order}")
-            for object in json_step: # repeats for every single step in the .json file
-                action_is_done = False
-                for key, value in object.items(): # this repeats for every element in each line
-                    print(f"key: {key}, value:{value}")
-                    # json_objects.append(f'{key}:{value}')
+        for object in json_step: # repeats for every single step in the .json file
+            action_is_done = False
+            for key, value in object.items(): # this repeats for every element in each line
+                print(f"key: {key}, value:{value}")
+                # json_objects.append(f'{key}:{value}')
+                for order in csv_rows: # first loops through the order (x2)
                     for element in order:
-                        print(f"element: {element}")
-                        # Start the loop for repeat orders
+
+                        # Handle multiple orders
                         if value == "Quantity Text Box":
                             for product in range(product_amount):
                                 type
                                 action_is_done = True
 
-
-                        # move mouse and type for the rest
-                        elif not key == "Name":
-                            if key == "Coordinates": # works
-                                move_mouse(value)
-                                time.sleep(0.2)
-                                break
-                            elif key == "Select All":
-                                for _ in range(9):
-                                    pyautogui.press('backspace')
-                                time.sleep(0.2)
-                                action_is_done = True
-                            elif not value:
-                                print(f"just about to print: {element}")
-                                type_keyboard(element)
-                                action_is_done = True
-                        else:
+                        elif not (key == "Name" and ticket_structure_is_done):
+                            make_ticket_structure(key, value, element)
                             break
-                if action_is_done:
-                    break
-                    
-            print(f"json_objects: {order}")
+                        else: 
+                            break
+
+            if action_is_done:
+                break
+                
+        print(f"json_objects: {order}")
 
 
 
